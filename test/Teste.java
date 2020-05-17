2265479e70bd6a231eb987d03043ab6389174b9f
Diego Barros <diegobarrosflf@gmail.com>
2020-05-16 23:56:50 -0300

    Criando usuarios

--- test/Teste.java
+++ test/Teste.java
@@ -1,7 +1,13 @@
 package test;
 
+import model.Usuario;
+
 public class Teste {
     public static void main(String[] args) {
+
+        Usuario user1 = new Usuario("Scorpion", 1000);
+        Usuario user2 = new Usuario("SubZero", 850);
+        Usuario user3 = new Usuario("Kabal", 950);
         
     }
 }
